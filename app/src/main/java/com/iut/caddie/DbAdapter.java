f27b306c48acf292f778e4fdd980de407e6989cd
DaruniaIsBack <maximevaneygen@gmail.com>
2022-03-29 16:30:28 +0200

    Code change comment

--- app/src/main/java/com/iut/caddie/DbAdapter.java
+++ app/src/main/java/com/iut/caddie/DbAdapter.java
@@ -28,14 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simple notes database access helper class. Defines the basic CRUD operations
- * for the notepad example, and gives the ability to list all notes as well as
- * retrieve or modify a specific note.
- *
- * This has been improved from the first version of this tutorial through the
- * addition of better error handling and also using returning a Cursor instead
- * of using a collection of inner classes (which is less scalable and not
- * recommended).
+ * dbAdapter create and edit database
  */
 public class DbAdapter {
 
@@ -69,7 +62,7 @@ public class DbAdapter {
     private static final String DATABASE_TABLE = "Products";
     private static final int DATABASE_VERSION = 2;
     /**
-     *  Products data
+     *  Products data start
      */
     private static String[] Products = {"lait", "orange","chocolat","jambon","frite"};
 
@@ -176,40 +169,11 @@ public class DbAdapter {
         return this;
     }
 
+    /**
+     * close acces to the database
+     */
     public void close() {
         mDbHelper.close();
-    }
-
-
-    /**
-     * Create a new note using the title and body provided. If the note is
-     * successfully created return the new rowId for that note, otherwise return
-     * a -1 to indicate failure.
-     *
-     * @param title the title of the note
-     * @return rowId or -1 if failed
-     */
-    public long createNote(String title) {
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_TITLE, title);
-
-        return mDb.insert(DATABASE_TABLE, null, initialValues);
-    }
-
-    /**
-     * Delete the note with the given rowId
-     *
-     * @param title name of the task
-     * @return true if deleted, false otherwise
-     */
-    public boolean deleteNote(String title) {
-
-        return mDb.delete(DATABASE_TABLE, KEY_TITLE + "=" +"'" + title + "'", null) > 0;
-    }
-
-    public boolean deleteAllNotes() {
-
-        return mDb.delete(DATABASE_TABLE, null, null) > 0;
     }
 
     /**
@@ -219,43 +183,6 @@ public class DbAdapter {
      */
     public Cursor fetchAllProducts() {
         return mDb.query(DATABASE_TABLE, new String[] {"_id", "produit"}, null, null, null, null, "produit");
-    }
-
-    /**
-     * Return a Cursor positioned at the note that matches the given rowId
-     *
-     * @param rowId id of note to retrieve
-     * @return Cursor positioned to matching note, if found
-     * @throws SQLException if note could not be found/retrieved
-     */
-    public Cursor fetchNote(long rowId) throws SQLException {
-
-        Cursor mCursor =
-
-                mDb.query(true, DATABASE_TABLE, new String[] {KEY_ROWID,
-                                KEY_TITLE}, KEY_ROWID + "=" + rowId, null,
-                        null, null, null, null);
-        if (mCursor != null) {
-            mCursor.moveToFirst();
-        }
-        return mCursor;
-
-    }
-
-    /**
-     * Update the note using the details provided. The note to be updated is
-     * specified using the rowId, and it is altered to use the title and body
-     * values passed in
-     *
-     * @param rowId id of note to update
-     * @param title value to set note title to
-     * @return true if the note was successfully updated, false otherwise
-     */
-    public boolean updateNote(long rowId, String title) {
-        ContentValues args = new ContentValues();
-        args.put(KEY_TITLE, title);
-
-        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
     }
 
     /**
