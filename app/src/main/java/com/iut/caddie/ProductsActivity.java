f6ac64c9b3d5ae091828ebbf32e7ee173970ba52
Plecauz <50369990+Plecauz@users.noreply.github.com>
2022-03-29 11:12:07 +0200

    mise en place des tests de la bdd

--- app/src/main/java/com/iut/caddie/ProductsActivity.java
+++ app/src/main/java/com/iut/caddie/ProductsActivity.java
@@ -2,13 +2,48 @@ package com.iut.caddie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductsActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private final List<String> products = new ArrayList<>();
+
+    private ArrayAdapter<String> listAdapter;
+
+    private DbAdapter bdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_products);
+
+        listView = findViewById(R.id.listProducts);
+
+        bdd = new DbAdapter(this);
+
+        bdd.open();
+
+        fillData();
+
+    }
+
+    void fillData(){
+        products.clear();
+        Cursor c = bdd.fetchAllNotes();
+        c.moveToFirst();
+        while(!c.isAfterLast()){
+            products.add(c.getString(c.getColumnIndexOrThrow("produit")));
+            c.moveToNext();
+        }
+        c.close();
+        listAdapter.notifyDataSetChanged();
     }
 }
