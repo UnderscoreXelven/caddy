916e5cdc78408ece09fe7d380d76c64a98c0045a
Plecauz <50369990+Plecauz@users.noreply.github.com>
2022-03-29 10:59:07 +0200

    debug mode
    ajout de la barre de navigation qui sert pour le moment à naviguer entre les tests d'activity

--- app/src/main/java/com/iut/caddie/ListActivity.java
+++ app/src/main/java/com/iut/caddie/ListActivity.java
@@ -2,9 +2,15 @@ package com.iut.caddie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +41,35 @@ public class ListActivity extends AppCompatActivity {
         listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,products);
 
         listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                products.remove(position);
+                listAdapter.notifyDataSetChanged();
+            }
+        });
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigation);
+
+        navigationBarView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case(R.id.goto_main):
+                    startActivity(new Intent(ListActivity.this, MainActivity.class));
+                    return true;
+                case(R.id.goto_list):
+                    startActivity(new Intent(ListActivity.this, ListActivity.class));
+                    return true;
+                case(R.id.goto_allList):
+                    startActivity(new Intent(ListActivity.this, AllList.class));
+                    return true;
+                case(R.id.goto_products):
+                    startActivity(new Intent(ListActivity.this, ProductsActivity.class));
+                    return true;
+                default:
+                    return false;
+            }
+        });
 
     }
 }
