b779352c296d8d872b99b942809c62527787de08
Plecauz <50369990+Plecauz@users.noreply.github.com>
2022-03-29 13:38:58 +0200

    Update ProductsActivity.java

--- app/src/main/java/com/iut/caddie/ProductsActivity.java
+++ app/src/main/java/com/iut/caddie/ProductsActivity.java
@@ -24,6 +24,7 @@ public class ProductsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+        setTitle("Liste de produits");
 
         listView = findViewById(R.id.listProducts);
 
