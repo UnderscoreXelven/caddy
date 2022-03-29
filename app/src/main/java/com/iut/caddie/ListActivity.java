d2457c22b49182c144341b1e127f98a9640304d7
Plecauz <50369990+Plecauz@users.noreply.github.com>
2022-03-29 09:56:32 +0200

    changement listActivity

--- app/src/main/java/com/iut/caddie/ListActivity.java
+++ app/src/main/java/com/iut/caddie/ListActivity.java
@@ -3,12 +3,37 @@ package com.iut.caddie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
+    private ListView listView;
+
+    private final List<String> products = new ArrayList<>();
+
+    private ArrayAdapter<String> listAdapter;
+
+    private DbAdapter bdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        products.add("ketchup");
+        products.add("mayo");
+        products.add("moutarde");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list);
+
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,products);
+
+        listView.setAdapter(listAdapter);
+
     }
 }
